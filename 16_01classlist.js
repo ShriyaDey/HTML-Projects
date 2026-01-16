9745a86fb703c1fd38b7f9100bf84a6386de1e7a
ShriyaDey <111898132+ShriyaDey@users.noreply.github.com>
2026-01-16 16:44:54 +0530

    Add files via upload

---
+++ 16_01classlist.js
@@ -0,0 +1,10 @@
+function myfunction(){
+    const element= document.getElementById("mydiv");
+    element.classList.add("mystyle");
+
+}
+function removefunction(){
+    const element=document.getElementById("mydiv");
+    element.classList.remove("mystyle");
+}
+
